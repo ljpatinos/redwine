9d0a5b2e72b25738d567628b1a3c639f29b192a0
Leydi <ljpatinos@unal.edu.co>
2025-02-06 17:00:55 -0500

    Update aplication.py

--- aplication.py
+++ aplication.py
@@ -12,33 +12,31 @@ def load_model():
 def main():
     # Título con color
     st.markdown(
-        '<h1 style="color: #4CAF50; text-align: center;">Predicción de Precios de Casas (Boston Housing)</h1>',
+        '<h1 style="color: #FFFFFF; text-align: center;">Predicción de la calidad del vino rojo </h1>',
         unsafe_allow_html=True
     )
 
-    st.markdown("El mejor modelo de regresión obtenido presentalas siguientes características KernelRidge, Scaler: StandardScaler, alpha=0.1, kernel='rbf' y MAE=0.7414")
+    st.markdown("El mejor modelo de regresión obtenido presentó las siguientes características KernelRidge, Scaler: StandardScaler, alpha=0.1, kernel='rbf' y MAE=0.7414")
     
-    st.markdown("Ingrese las características de la casa para predecir su precio:")
+    st.markdown("Ingrese las características de la casa para predecir la calidad:")
     
     # Entradas para las 13 características del conjunto de datos Boston Housing
-    crim = st.number_input("Tasa de criminalidad per cápita por ciudad", min_value=0.0, format="%.5f")
-    zn = st.number_input("Proporción de terreno residencial zonificado para lotes grandes", min_value=0.0, format="%.2f")
-    indus = st.number_input("Proporción de acres comerciales por ciudad", min_value=0.0, format="%.2f")
-    chas = st.selectbox("Variable ficticia Charles River (1: colinda, 0: no colinda)", [0, 1])
-    nox = st.number_input("Concentración de óxidos de nitrógeno (partes por 10 millones)", min_value=0.0, format="%.3f")
-    rm = st.number_input("Número promedio de habitaciones por vivienda", min_value=1.0, format="%.2f")
-    age = st.number_input("Proporción de viviendas construidas antes de 1940", min_value=0.0, format="%.1f")
-    dis = st.number_input("Distancia ponderada a cinco centros de empleo de Boston", min_value=0.0, format="%.3f")
-    rad = st.number_input("Índice de accesibilidad a carreteras radiales", min_value=1, max_value=24, step=1)
-    tax = st.number_input("Tasa de impuesto a la propiedad por cada $10,000", min_value=0.0, format="%.1f")
-    ptratio = st.number_input("Índice alumno-profesor por ciudad", min_value=0.0, format="%.2f")
-    b = st.number_input("Proporción de población afroamericana por ciudad", min_value=0.0, format="%.2f")
-    lstat = st.number_input("Porcentaje de población de bajos ingresos", min_value=0.0, format="%.2f")
-    
+    fixed acidity = st.number_input("Acidez fija", min_value=0.0, format="%.5f")
+    volatile acidity = st.number_input("Ácidez volatil", min_value=0.0, format="%.2f")
+    citric acid = st.number_input("Ácido cítrico", min_value=0.0, format="%.2f")
+    residual sugar = st.number_input("Azucar residual", min_value=0.0, format="%.2f")
+    chlorides = st.number_input("Cloruros", min_value=0.0, format="%.3f")
+    free sulfur dioxide = st.number_input("Dioxido de azufre libre", min_value=1.0, format="%.2f")
+    total sulfur dioxide = st.number_input("Dioxido de azufre total", min_value=0.0, format="%.1f")
+    density = st.number_input("Densidad", min_value=0.0, format="%.3f")
+    pH = st.number_input("pH", min_value=0.0, format="%.1f")
+    sulphates = st.number_input("Sulfatos", min_value=0.0, format="%.1f")
+    alcohol = st.number_input("Contenido de alcohol (%)", min_value=0.0, format="%.2f")
+        
     # Botón de predicción
     if st.button("Predecir Precio"):
         model = load_model()
-        input_data = np.array([[crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, b, lstat]])
+        input_data = np.array([[fixed acidity, volatile acidity, citric acid, residual sugar, chlorides, free sulfur dioxide, total sulfur dioxide, density, pH, sulphates, alcohol]])
         prediction = model.predict(input_data)
         st.markdown(f"### Precio estimado de la vivienda: **${prediction}**")
 

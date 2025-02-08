7dfbc1b083ea25bba367860724b0c1b16ed84bfe
Leydi <ljpatinos@unal.edu.co>
2025-02-08 11:09:42 -0500

    Update aplication.py

--- aplication.py
+++ aplication.py
@@ -4,7 +4,7 @@ import pickle
 import gzip
 
 def load_model():
-    filename = "model_trained_regressor.pkl.gz"
+    filename = "best_model.pkl.gz"
     with gzip.open(filename, 'rb') as f:
         model = pickle.load(f)
     return model

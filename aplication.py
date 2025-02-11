b447225f300dbd9f8727956e86e155808d1fab28
Leydi <ljpatinos@unal.edu.co>
2025-02-11 18:16:11 -0500

    Update aplication.py

--- aplication.py
+++ aplication.py
@@ -24,7 +24,7 @@ st.markdown(
         }
         h2 { color: #FFFFFF; font-size: 24px; }
         h3, h4, h5, h6 { color: #FFFFFF; font-size: 18px; }
-        .stSidebar { background-color: #561B47; }
+        .stSidebar { background-color: #000080; }
         .dataframe { margin: auto; }
     </style>
     """, 
